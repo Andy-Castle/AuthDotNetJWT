9d79f7c52d875ddb1401ef9e53ef4bd85aa44d72
Andy Castle <masterchief_and@live.com>
2025-02-10 16:58:07 -0500

    Se realizo la migraciones del modelo de la base de datos, tambien se creo el servicio para crear un usuario

--- AuthDotNetJWT/Services/AuthService.cs
+++ AuthDotNetJWT/Services/AuthService.cs
@@ -1,14 +1,48 @@
-﻿using AuthDotNetJWT.Entities;
+﻿using AuthDotNetJWT.Data;
+using AuthDotNetJWT.Entities;
 using AuthDotNetJWT.Models;
+using Azure.Core;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace AuthDotNetJWT.Services
 {
-    public class AuthService(DbContext context, IConfiguration configuration) : IAuthService
+    public class AuthService(ApplicationDbContext context, IConfiguration configuration) : IAuthService
     {
-        public Task<User> Create(CreateUserDto createUserDto)
+
+        public async Task<User> Create(CreateUserDto createUserDto)
         {
-            throw new NotImplementedException();
+            try
+            {
+
+                if (await context.Users.AnyAsync(u => u.Email == createUserDto.Email))
+                {
+                    throw new BadHttpRequestException($"{createUserDto.Email} ya existe");
+                }
+
+                var user = new User
+                {
+                    Name = createUserDto.Name,
+                    Email = createUserDto.Email
+                };
+
+                var hashedPassword = new PasswordHasher<User>()
+                        .HashPassword(user, createUserDto.Password);
+
+                user.Password = hashedPassword;
+
+
+                context.Users.Add(user);
+                await context.SaveChangesAsync();
+
+                return user;
+            }
+            catch (Exception ex) when (ex is not BadHttpRequestException)
+            {
+                string message = "Error al crear el usuario";
+
+                throw new Exception($"Salio algo mal, {message}", ex);
+            }
         }
 
         public Task<IEnumerable<User>> FindAll()
